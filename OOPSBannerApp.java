61e9bbe5c41e5a0efd3914eef4f3d7231b1d48c8
Deepak saini <deepaksaini15072007@gmail.com>
2026-02-23 13:50:01 +0530

    fest: add uc6

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+public class OOPSBannerUC4{
+	public static void main(String[] args){
+		lines[0] = System.out.println(string.join(" ", "  ***  ", "  ***** ");
+		lines[1] = System.out.println(string.join(" ", " ** ** ", " **     ");
+		lines[2] = System.out.println(string.join(" ", "**    **","**      ");
+		lines[3] = System.out.println(string.join(" ", "**    **"," *****  ");
+		lines[4] = System.out.println(string.join(" ", "**    **", "    ** ");	
+		lines[5] = System.out.println(string.join(" ", " ** ** ", " **  ** ");	
+		lines[6] = System.out.println(string.join(" ", "  ***  ", "  ***** ");	
+		for (string line : lines) {
+			system.out.println(line);
+		}	
+	}
+}
